1d0c6463c1304db69fcdecd338eea0f0516b67a9
soroush asadi <soroushdes@hotmail.com>
2020-01-26 14:46:54 +0330

    Add project files.

---
+++ BussinessLayer/Entities/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BussinessLayer.Entities
+{
+    public class ApplicationUser:IdentityUser
+    {
+
+        public string FullName { get; set; }
+        public DateTime  RegisterDate { get; set; } = DateTime.Now;
+        public DateTime LastOnlineDate { get; set; } = DateTime.Now;
+        public DateTime? LastPasswordResetDate { get; set; } 
+    }
+}
---
+++ SAPClearArchitectureTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SAPClearArchitectureTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
